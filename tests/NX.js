b00aa789a4ba42c6d27ed9e22cc677da4eb1f924
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-11-26 21:46:43 +0900

    テストメソッド実行までできた。

--- tests/NX.js
+++ tests/NX.js
@@ -1,7 +1,41 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * xFramework NX
+ * Copyright(c) 2006-2010 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
+
+// {{{ NXTestCase
+
+/**
+ * @class NXTestCase
+ *
+ * NXクラステストケース
+ */
+NXTestCase = NX.extend(NX.test.unit.TestCase, {
+
+    // {{{ testIsString
+
+    testIsString : function() {
 
 
-NXTestCase = NX.extend(Object, {
 
+
+
+
+    }
+
+    // }}}
 
 });
 
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
