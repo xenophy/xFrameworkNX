da6a85259a3b2879e3655b28d6e96e68fce0857d
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-12-01 18:30:35 +0900

    NX.sleep追加

---
+++ tests/util/DelayedTask.js
@@ -0,0 +1,43 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * xFramework NX
+ * Copyright(c) 2006-2010 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
+
+// {{{ NX.util.DelayedTaskTestCase
+
+/**
+ * @class NX.util.DelayedTaskTestCase
+ *
+ * NXクラステストケース
+ */
+NX.util.DelayedTaskTestCase = NX.extend(NX.test.unit.TestCase, {
+
+    // {{{ testDelay
+
+    /**
+     * NX.util.DelayedTask.delayテスト
+     */
+    testDelay : function() {
+
+        var me = this;
+
+
+        me.assertOk(NX.isEmpty({}) === false);
+    },
+
+    // }}}
+
+});
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
