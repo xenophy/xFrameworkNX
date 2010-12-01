8af604938d5c0d7e2a36d9a96d710f7d759cbe8f
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-12-02 05:58:52 +0900

    NX.util.Observableクラスメソッド一通り実装完了

--- tests/util/Observable.js
+++ tests/util/Observable.js
@@ -41,7 +41,7 @@ NX.util.ObservableTestCase = NX.extend(NX.test.unit.TestCase, {
         v = undefined;
         o.on('event2', f);
         o.fireEvent('event2', 'Fired Event2');
-        me.assertStrictEqual(v, undefined);
+        me.assertStrictEqual(v, 'Fired Event2');
 
         o.addEvents('event3');
 
@@ -128,7 +128,6 @@ NX.util.ObservableTestCase = NX.extend(NX.test.unit.TestCase, {
         ret = t.fn.apply(t.scope);
 
         me.assertStrictEqual(ret, 'testevent');
-
 
         var o = new NX.util.Observable({
             listeners : {
@@ -272,6 +271,237 @@ NX.util.ObservableTestCase = NX.extend(NX.test.unit.TestCase, {
 
         o.fireEvent('event3', 'Event1');
         me.assertStrictEqual(v, 'Event3');
+
+    },
+
+    // }}}
+    // {{{ testSuspendEvents
+
+    /**
+     * NX.util.Observable.suspendEventsテスト
+     */
+    testSuspendEvents : function() {
+
+        var me = this;
+
+        var o = new NX.util.Observable();
+        var v;
+        var f = function(value) {
+            v = value;
+        }
+
+        o.on('event1', f);
+        o.on('event2', f);
+        o.on('event3', f);
+
+        o.fireEvent('event1', 'Event1');
+        me.assertStrictEqual(v, 'Event1');
+
+        o.suspendEvents();
+
+        o.fireEvent('event2', 'Event2');
+        me.assertStrictEqual(v, 'Event1');
+
+        o.fireEvent('event3', 'Event3');
+        me.assertStrictEqual(v, 'Event1');
+
+    },
+
+    // }}}
+    // {{{ testResumeEvents
+
+    /**
+     * NX.util.Observable.resumeEventsテスト
+     */
+    testResumeEvents : function() {
+
+        var me = this;
+
+        var o = new NX.util.Observable();
+        var v;
+        var f = function(value) {
+            v = value;
+        }
+
+        o.on('event1', f);
+        o.on('event2', f);
+        o.on('event3', f);
+
+        o.fireEvent('event1', 'Event1');
+        me.assertStrictEqual(v, 'Event1');
+
+        o.suspendEvents();
+
+        o.fireEvent('event2', 'Event2');
+        me.assertStrictEqual(v, 'Event1');
+
+        o.fireEvent('event3', 'Event3');
+        me.assertStrictEqual(v, 'Event1');
+
+        o.resumeEvents();
+
+        o.fireEvent('event2', 'Event2');
+        me.assertStrictEqual(v, 'Event2');
+
+        o.fireEvent('event3', 'Event3');
+        me.assertStrictEqual(v, 'Event3');
+
+        o.fireEvent('event1', 'Event1');
+        me.assertStrictEqual(v, 'Event1');
+
+        o.suspendEvents(true);
+
+        o.fireEvent('event2', 'Event2');
+        me.assertStrictEqual(v, 'Event1');
+
+        o.fireEvent('event3', 'Event3');
+        me.assertStrictEqual(v, 'Event1');
+
+        o.resumeEvents();
+
+        me.assertStrictEqual(v, 'Event3');
+
+    },
+
+    // }}}
+    // {{{ testRelayEvents
+
+    /**
+     * NX.util.Observable.relayEventsテスト
+     */
+    testRelayEvents : function() {
+
+        var me = this;
+
+        var op = new NX.util.Observable();
+        var o = new NX.util.Observable();
+        var v;
+        var fp = function(value) {
+            v = 'Parent! ' + value;
+        }
+        var f = function(value) {
+            v = value;
+        }
+
+        op.addEvents('parentevent');
+
+        op.on('parentevent', fp);
+
+        o.relayEvents(op, ['parentevent']);
+
+        o.on('parentevent', f);
+
+        op.fireEvent('parentevent', 'fired');
+
+        me.assertStrictEqual(v, 'fired');
+
+    },
+
+    // }}}
+    // {{{ testEnableBubble
+
+    /**
+     * NX.util.Observable.enableBubbleテスト
+     */
+    testEnableBubble : function() {
+
+        var me = this;
+
+        var op = new NX.util.Observable();
+        var o = new NX.util.Observable();
+        var v;
+
+        o.enableBubble('event1');
+
+        o.getBubbleTarget = function() {
+            return op;
+        };
+
+        op.on('event1', function() {
+            v = 'event1 called';
+        });
+
+        o.fireEvent('event1');
+
+        me.assertStrictEqual(v, 'event1 called');
+
+    },
+
+    // }}}
+    // {{{ testCapture
+
+    /**
+     * NX.util.Observable.testCaptureテスト
+     */
+    testCapture : function() {
+
+        var me = this;
+
+        var o = new NX.util.Observable();
+        var v;
+
+        NX.util.Observable.capture(o, function(ename, value) {
+            v = value;
+        });
+
+        o.fireEvent('event1', 'event1 called');
+
+        me.assertStrictEqual(v, 'event1 called');
+
+    },
+
+    // }}}
+    // {{{ testReleaseCapture
+
+    /**
+     * NX.util.Observable.testReleaseCaptureテスト
+     */
+    testReleaseCapture : function() {
+
+        var me = this;
+
+        var o = new NX.util.Observable();
+        var v;
+        var f = function(ename, value) {
+            v = value;
+        }
+
+        NX.util.Observable.capture(o, f);
+
+        o.fireEvent('event1', 'event1 called');
+
+        me.assertStrictEqual(v, 'event1 called');
+
+        v = undefined;
+        NX.util.Observable.releaseCapture(o, f);
+
+        o.fireEvent('event1', 'event1 called');
+
+        me.assertStrictEqual(v, undefined);
+    },
+
+    // }}}
+    // {{{ testObserve
+
+    /**
+     * NX.util.Observable.observeテスト
+     */
+    testObserve : function() {
+
+        var me = this;
+        var o = {};
+        var v;
+
+        NX.util.Observable.observe(o, {
+            'event1' : function() {
+                v = 'event1 called';
+            }
+        });
+
+        o.fireEvent('event1');
+
+        me.assertStrictEqual(v, 'event1 called');
+
     }
 
     // }}}
