f291fff2ebe6322f30d3224cbad065237b8a0887
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-12-05 08:17:14 +0900

    ドキュメント用Extアプリ作成開始

---
+++ src/widgets/HeaderBoxComponent.js
@@ -0,0 +1,50 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * xFramework NX Docs
+ *
+ * http://xFrameworknx.com
+ */
+
+// {{{ Ext.HeaderBoxComponent
+
+/**
+ * Ext.HeaderBoxComponent
+ *
+ * @author  Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
+ */
+Ext.HeaderBoxComponent = Ext.extend(Ext.BoxComponent, {
+
+    // {{{ initComponent
+
+    initComponent : function() {
+
+        var me = this;
+
+        // 設定適用
+        Ext.apply(me, {
+            html: 'test'
+        });
+
+        // スーパークラスメソッドコール
+        Ext.HeaderBoxComponent.superclass.initComponent.apply(me, arguments);
+    }
+
+    // }}}
+
+});
+
+// }}}
+// {{{ register xtype
+
+Ext.reg('header', Ext.HeaderBoxComponent);
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
