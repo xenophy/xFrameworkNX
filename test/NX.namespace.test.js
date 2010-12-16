24aa58bd419dffee5ad863f8d9e215dc53f51914
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-12-16 22:45:23 +0900

    テストケース実装予定ファイル追加

---
+++ test/NX.namespace.test.js
@@ -0,0 +1,39 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * xFramework NX
+ * Copyright(c) 2006-2011 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
+
+// {{{ requires
+
+var NX = require('NX'),
+    assert = require('assert');
+
+// }}}
+// {{{ NX Class Tests
+
+module.exports = {
+
+    // {{{ test namespace#standard
+
+    'test namespace#standard': function() {
+
+        // TODO: careate test.
+
+    }
+
+    // }}}
+
+};
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
