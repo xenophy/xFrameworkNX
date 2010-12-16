af1c2dcdac89069409bc5325f021e0e6e6972f50
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-12-16 11:08:47 +0900

    expressoで動くテストケースの記述開始

---
+++ test/NX.test.js
@@ -0,0 +1,36 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/*!
+ * xFramework NX
+ * Copyright(c) 2006-2011 Xenophy.CO.,LTD All rights Reserved.
+ * http://www.xenophy.com
+ */
+
+// {{{ requires
+
+var NX = require('../lib/NX'),
+    assert = require('assert');
+
+
+// }}}
+// {{{ NX Class Tests
+
+module.exports = {
+
+    'test version': function(){
+
+        assert.ok(/^\d+\.\d+\.\d+$/.test(NX.version), 'Test framework version format');
+
+    },
+
+};
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
