5ae2cbc517ecf710cecba3a969ab5fcfdca81f53
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-12-23 04:33:58 +0900

    現時点でコードカバレッジ100%達成

--- test/NX.extend.test.js
+++ test/NX.extend.test.js
@@ -20,7 +20,113 @@ module.exports = {
 
     'test extend#standard': function() {
 
-        // TODO: careate test.
+        var NewCls = NX.extend(Object, {
+            prop1: 'prop1',
+            method1: function() {
+                return 'method1';
+            }
+        });
+
+        var cls = new NewCls();
+
+        assert.equal(cls.prop1, 'prop1');
+        assert.equal(cls.method1(), 'method1');
+
+        var SubCls = NX.extend(NewCls, {
+            prop2: 'prop2',
+            method1: function() {
+                return 'override method';
+            }
+        });
+
+        var sub = new SubCls();
+
+        SubCls.prototype.override({
+            method2: function() {
+                return 'inline override'
+            }
+        });
+
+        assert.equal(sub.prop1, 'prop1');
+        assert.equal(sub.prop2, 'prop2');
+        assert.equal(sub.method1(), 'override method');
+        assert.equal(sub.method2(), 'inline override');
+
+    },
+
+    // }}}
+    // {{{ test extend#anotherstyle
+
+    'test extend#anotherstyle': function() {
+
+        var NewCls = function() {
+            // NewCls constructor
+            return 'newcls constructor';
+        };
+        NX.extend(NewCls, Object, {
+            prop1: 'prop1',
+            method1: function() {
+                return 'method1';
+            }
+        });
+
+        var cls = new NewCls();
+
+        assert.equal(cls.prop1, 'prop1');
+        assert.equal(cls.method1(), 'method1');
+
+        var SubCls = function() {
+            // SubCls constructor
+            return 'subcls constructor';
+        };
+
+        NX.extend(SubCls, NewCls, {
+            prop2: 'prop2',
+            method1: function() {
+                return 'override method';
+            }
+        });
+
+        var sub = new SubCls();
+
+        assert.equal(sub.prop1, 'prop1');
+        assert.equal(sub.prop2, 'prop2');
+        assert.equal(sub.method1(), 'override method');
+
+        var ret = SubCls.prototype.superclass();
+        ret = ret.constructor();
+
+        assert.equal(ret, 'newcls constructor');
+
+        var GsonCls = SubCls.extend({
+            method2: function() {
+                return 'gson method';
+            }
+        });
+
+        var gson = new GsonCls();
+
+        assert.equal(gson.prop1, 'prop1');
+        assert.equal(gson.prop2, 'prop2');
+        assert.equal(gson.method1(), 'override method');
+        assert.equal(gson.method2(), 'gson method');
+
+    },
+
+    // }}}
+    // {{{ test extend#illegal
+
+    'test extend#illegal': function() {
+
+        var NewCls = function() {
+            // NewCls constructor
+        };
+
+        try {
+            NX.extend(NewCls);
+        } catch(e) {
+            assert.equal(e, 'The specified superclass object is illegal.');
+        }
 
     }
 
